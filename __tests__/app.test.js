79f4e21f9cb3501163db861ba951b0250571a890
Julianne Vela <75726146+julianne-vela@users.noreply.github>
2021-04-06 17:29:26 -0700

    first POST test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,19 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('twitter-toxicity routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+describe('twit-bot CRUD routes', () => {
+	beforeEach(() => {
+		return setup(pool);
+	});
+
+	it('takes in a username and adds a new user to the DB', async () => {
+		const res = await request(app)
+			.post('/api/v1/users')
+			.send({ userName: 'NessimaSkye' });
+
+		expect(res.body).toEqual({
+			id: expect.any(String),
+			user_name: 'NessimaSkye',
+		});
+	});
 });
