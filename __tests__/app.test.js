6bffe27172e8a6ecb2e3b433726fa9090b7a4a2f
Julianne Vela <75726146+julianne-vela@users.noreply.github>
2021-04-07 08:44:43 -0700

    GET all tweets test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('twit-bot CRRUD routes', () => {
+describe('twit-bot User CRRUD routes', () => {
 	beforeEach(() => {
 		return setup(pool);
 	});
@@ -65,5 +65,36 @@ describe('twit-bot CRRUD routes', () => {
 			id: '1',
 			userName: 'NessimaSkye',
 		});
+	});
+});
+
+describe('twit-bot Tweet CRRUD routes', () => {
+	beforeEach(() => {
+		return setup(pool);
+	});
+
+	beforeEach(async () => {
+		const newUser = {
+			userName: 'NessimaSkye',
+		};
+		const newTweet = {
+			tweet: 'This is my first tweet',
+			linkedUser: '1',
+		};
+
+		await request(app).post('/api/v1/users').send(newUser);
+		await request(app).post('/api/v1/tweets').send(newTweet);
+	});
+
+	it('GET: returns all tweets in the DB', async () => {
+		const { body } = await request(app).get('/api/v1/tweets');
+
+		expect(body).toEqual([
+			{
+				id: expect.any(String),
+				tweet: 'This is my first tweet',
+				linkedUser: '1',
+			},
+		]);
 	});
 });
